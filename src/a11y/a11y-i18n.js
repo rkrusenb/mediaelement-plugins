1f7a82fa605bd9badffdcf3164acd8255dc58660
Raphael Krusenbaum <rkrusenb-git@noreply.materna.group>
2026-09-17 20:01:49 +0200

    fix minor a11y problem with German button labels

--- src/a11y/a11y-i18n.js
+++ src/a11y/a11y-i18n.js
@@ -1,8 +1,8 @@
 'use strict';
 
 if (mejs.i18n.de !== undefined) {
-    mejs.i18n.de['mejs.a11y-audio-description'] = 'Audio Deskription An/Aus';
-    mejs.i18n.de['mejs.a11y-video-description'] = 'Video Deskription An/Aus';
+    mejs.i18n.de['mejs.a11y-audio-description'] = 'Audiodeskription An/Aus';
+    mejs.i18n.de['mejs.a11y-video-description'] = 'Videodeskription An/Aus';
 }
 
 if (mejs.i18n.fr !== undefined) {
